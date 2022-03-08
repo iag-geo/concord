f856e386de485df10b31ac3a8d67ed7a031bc054
Hugh Saalmans <hugh.saalmans@iag.com.au>
2022-03-08 14:18:40 +1100

    testing GNAF

--- xx_testing.sql
+++ xx_testing.sql
@@ -4,9 +4,11 @@ select bdy.state,
        bdy.lga_name,
        bdy.postcode,
        bdy.locality_name,
-       count(gnaf_pid) as addr_count
-from gnaf_202202.address_alias_admin_boundaries as bdy
--- where  = 'RESIDENTIAL'
+       count(bdy.gnaf_pid) as addr_count
+from gnaf_202202.address_principal_admin_boundaries as bdy
+inner join gnaf_202202.address_principals as gnaf on gnaf.gnaf_pid = bdy.gnaf_pid
+inner join admin_bdys_202202.abs_2016_mb as mb on mb.mb_16code = gnaf.mb_2016_code
+where mb.mb_category = 'RESIDENTIAL'
 group by bdy.state,
          bdy.lga_name,
          bdy.postcode,
@@ -16,11 +18,20 @@ order by bdy.state,
          bdy.postcode,
          bdy.locality_name,
          addr_count desc
+LIMIT 50
 ;
 
 
 
 select *
+from gnaf_202202.address_principals
+;
+
+select *
 from gnaf_202202.address_alias_admin_boundaries
+;
+
+select *
+from admin_bdys_202202.abs_2016_mb
 ;
 
