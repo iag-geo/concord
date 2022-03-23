26c75aa8d595617f6e45d729f9cc71eb3804b174
Hugh Saalmans <hugh.saalmans@iag.com.au>
2022-03-23 17:58:47 +1100

    working on MB based population projection

--- xx_lga_testing.sql
+++ xx_lga_testing.sql
@@ -11,9 +11,11 @@
 -- vacuum analyse census_2016_bdys.poa_2016_aust;
 
 
--- projet current population from changes to address counts since 201708 and test against 2021 LGA population data
+-- project current population from changes to address counts since 201708 and test against 2021 LGA population data
 
 -- step 1 - add 201708 and 220202 address counts to 2016 meshblocks
+drop table if exists testing.mb_2016_counts_2022;
+create table testing.mb_2016_counts_2022 as
 with gnaf_2017 as (
     select mb_2016_code,
            count(*) as address_count
@@ -24,35 +26,66 @@ with gnaf_2017 as (
            count(*) as address_count
     from gnaf_202202.address_principals
     group by mb_2016_code
-), merge as (
-    select mb.mb_2016_code,
-           mb_category_name_2016,
-           area_albers_sqkm,
-           dwelling,
-           person,
-           mb.address_count,
-           coalesce(gnaf_2017.address_count, 0) as address_count_201708,
-           coalesce(gnaf_2022.address_count, 0) as address_count_202202,
-           0::integer                           as person_202202,
-           state,
-           geom
-    from testing.mb_2016_counts as mb
-             left outer join gnaf_2017 on gnaf_2017.mb_2016_code = mb.mb_2016_code
-             left outer join gnaf_2022 on gnaf_2022.mb_2016_code = mb.mb_2016_code
 )
+select mb.mb_2016_code,
+       mb_category_name_2016,
+       area_albers_sqkm,
+       dwelling,
+       person,
+       mb.address_count,
+       coalesce(gnaf_2017.address_count, 0) as  address_count_201708,
+       coalesce(gnaf_2022.address_count, 0) as  address_count_202202,
+       0::integer as person_202202,
+       state,
+       geom
+from testing.mb_2016_counts as mb
+left outer join gnaf_2017 on gnaf_2017.mb_2016_code = mb.mb_2016_code
+left outer join gnaf_2022 on gnaf_2022.mb_2016_code = mb.mb_2016_code
+;
+analyse testing.mb_2016_counts_2022;
+
+-- addd projected population
+update testing.mb_2016_counts_2022
+    set person_202202 = ((person::float / address_count_201708::float) * address_count_202202)::integer
+where address_count_201708 > 0
+;
+analyse testing.mb_2016_counts_2022;
+
+ALTER TABLE testing.mb_2016_counts_2022 ADD CONSTRAINT mb_2016_counts_2022_pkey PRIMARY KEY (mb_2016_code);
+create index mb_2016_counts_2022_geom_idx on testing.mb_2016_counts_2022 using gist (geom);
+alter table testing.mb_2016_counts_2022 cluster on mb_2016_counts_2022_geom_idx;
 
 
-
+-- aggregate meshbocks by LGA (using centroid)
+drop table if exists testing.lga_pop;
+create table testing.lga_pop as
+with mb as (
+    select mb_2016_code,
+           person,
+           person_202202,
+           ST_PointOnSurface(geom)         as geom
+    from testing.mb_2016_counts_2022
+)
+select lga_code16,
+       lga_name16,
+       ste_code16,
+       sum(person) as person,
+       sum(person_202202) as person_202202
+from mb
+inner join census_2016_bdys.lga_2016_aust as abs_lga on st_intersects(mb.geom, abs_lga.geom)
+group by lga_code16,
+         lga_name16,
+         ste_code16
 
 ;
+analyse testing.lga_pop;
 
 
-
-
-
-
-
-
+select *
+from testing.lga_pop
+order by ste_code16,
+         lga_name16
+;
 
 
 
