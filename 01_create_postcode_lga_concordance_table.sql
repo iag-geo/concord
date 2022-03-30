c8b47a4eb0c69ec6a3ba511d49a9ea2ebed3434b
Hugh Saalmans <hugh.saalmans@iag.com.au>
2022-03-30 15:53:19 +1100

    added postcode to lga code

--- 01_create_postcode_lga_concordance_table.sql
+++ 01_create_postcode_lga_concordance_table.sql
@@ -1,22 +1,17 @@
 
 -- create concordance table using addresses as a residential population proxy (yes, it's flawed but close)
 
--- step 1 of 2 -- get both boundary IDs for each GNAFPID -- 14,451,352 rows affected in 4 m 29 s 11 ms
+
+-- step 1 of 2 -- get both boundary IDs for each GNAFPID -- 14,451,352 rows affected in 38 s 631 ms
 -- If ABS LGAs or Postcodes - use meshblock centroids to assign addresses to ABS LGAs or Postcodes
 drop table if exists temp_bdy_concordance;
 create temporary table temp_bdy_concordance as
-with mb as (
-    select mb_2016_code,
-           ST_PointOnSurface(geom) as geom
-    from testing.mb_2016_counts_2022
-), source as (
+with source as (
     select gnaf_pid,
-           lga_code16                      as source_id,
-           split_part(lga_name16, ' (', 1) as source_name,
-           state                           as source_state
-    from mb
-    inner join census_2016_bdys.lga_2016_aust as abs_lga on st_intersects(mb.geom, abs_lga.geom)
-    inner join gnaf_202202.address_principals as gnaf on gnaf.mb_2016_code = mb.mb_2016_code
+           postcode as source_id,
+           postcode as source_name,
+           state    as source_state
+    from gnaf_202202.address_principals as gnaf
 )
 select source.gnaf_pid,
        source_id,
@@ -34,8 +29,9 @@ analyse temp_bdy_concordance;
 
 -- step 2 of 2 -- aggregate addresses and determine % overlap between all bdys.
 -- This is the % that will be applied to all datasets being converted between bdys
-drop table if exists testing.concordance;
-create table testing.concordance as
+-- drop table if exists testing.concordance;
+-- create table testing.concordance as
+insert into testing.concordance
 with source_counts as (
     select source_id,
            count(*) as address_count
@@ -62,7 +58,7 @@ with source_counts as (
              target_name,
              target_state
 ), final as (
-    select 'abs lga'::text as source_type,
+    select 'geoscape postcode'::text as source_type,
            agg.source_id,
            agg.source_name,
            agg.source_state,
@@ -77,28 +73,33 @@ with source_counts as (
 --            (agg.address_count::float / target_counts.address_count::float * 100.0)::smallint as percent_target_addresses
     from agg
              inner join source_counts on source_counts.source_id = agg.source_id
---              inner join target_counts on target_counts.target_id = agg.target_id
-), target as (
+--              inner join target_counts on target_counts.target_id = lga.target_id
+), source_bdys as (
+    select postcode,
+           st_collect(geom) as geom
+    from admin_bdys_202202.postcode_bdys_analysis
+    group by postcode
+), target_bdys as (
     select lga_pid,
            st_collect(geom) as geom
-    from admin_bdys_202202.local_government_areas
+    from admin_bdys_202202.local_government_areas_analysis
     group by lga_pid
 )
 select final.*,
-       st_intersection(source.geom, target.geom) as geom
+       st_intersection(source_bdys.geom, target_bdys.geom) as geom
 from final
-         inner join census_2016_bdys.lga_2016_aust as source on final.source_id = source.lga_code16
-         inner join target on final.target_id = target.lga_pid
+         inner join source_bdys on final.source_id = source_bdys.postcode
+         inner join target_bdys on final.target_id = target_bdys.lga_pid
 where percent_source_addresses > 0
 --    or percent_target_addresses > 0
 ;
 analyse testing.concordance;
 
-ALTER TABLE testing.concordance ADD CONSTRAINT concordance_pkey PRIMARY KEY (source_id, target_id);
-create index concordance_geom_idx on testing.concordance using gist (geom);
-alter table testing.concordance cluster on concordance_geom_idx;
+-- ALTER TABLE testing.concordance ADD CONSTRAINT concordance_pkey PRIMARY KEY (source_id, target_id);
+-- create index concordance_geom_idx on testing.concordance using gist (geom);
+-- alter table testing.concordance cluster on concordance_geom_idx;
 
-drop table if exists temp_bdy_concordance;
+-- drop table if exists temp_bdy_concordance;
 
 
 select count(*)
