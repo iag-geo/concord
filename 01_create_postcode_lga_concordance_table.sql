d7c464558d5fb36e0165b69dc88e603a290b949c
Hugh Saalmans <hugh.saalmans@gmail.com>
2022-04-06 17:05:33 +1000

    testing windowing for percents

--- 01_create_postcode_lga_concordance_table.sql
+++ 01_create_postcode_lga_concordance_table.sql
@@ -6,22 +6,15 @@
 -- If ABS LGAs or Postcodes - use meshblock centroids to assign addresses to ABS LGAs or Postcodes
 drop table if exists temp_bdy_concordance;
 create temporary table temp_bdy_concordance as
-with source as (
-    select gnaf_pid,
-           postcode as source_id,
-           concat(state, ' ', postcode) as source_name,
-           state    as source_state
-    from gnaf_202202.address_principals as gnaf
-)
 select source.gnaf_pid,
-       source_id,
-       source_name,
-       source_state,
-       lga_pid as target_id,
-       lga_name as target_name,
-       state as target_state
-from gnaf_202202.address_principal_admin_boundaries as psma
-         inner join source on source.gnaf_pid = psma.gnaf_pid
+       source.postcode as source_id,
+       concat(source.state, ' ', source.postcode) as source_name,
+       source.state    as source_state,
+       target.lga_pid as target_id,
+       target.lga_name as target_name,
+       target.state as target_state
+from gnaf_202202.address_principal_admin_boundaries as target
+inner join gnaf_202202.address_principals as source on source.gnaf_pid = target.gnaf_pid
 ;
 analyse temp_bdy_concordance;
 
@@ -135,7 +128,9 @@ with source_counts as (
            target_id,
            target_name,
            target_state,
-           count(*) as address_count
+           count(*) as address_count,
+	       count(*) over (partition by source_id) as fred,
+           (count(*)::float / (count(*) over (partition by source_id))::float * 100.0) as percent_source_addresses_new
     from temp_bdy_concordance
     group by source_id,
              source_name,
@@ -154,30 +149,31 @@ with source_counts as (
            agg.target_state,
            agg.address_count,
            source_counts.address_count                                                        as total_source_addresses,
-           (agg.address_count::float / source_counts.address_count::float * 100.0)::smallint  as percent_source_addresses
+           (agg.address_count::float / source_counts.address_count::float * 100.0)::smallint  as percent_source_addresses,
+           agg.percent_source_addresses_new
 --            target_counts.address_count                                                       as total_target_addresses,
 --            (agg.address_count::float / target_counts.address_count::float * 100.0)::smallint as percent_target_addresses
     from agg
     inner join source_counts on source_counts.source_id = agg.source_id
 --              inner join target_counts on target_counts.target_id = lga.target_id
-), source_bdys as (
-    select postcode,
-           st_collect(geom) as geom
-    from admin_bdys_202202.postcode_bdys
-    where postcode is not null
-    group by postcode
-), target_bdys as (
-    select lga_pid,
-           st_collect(geom) as geom
-    from admin_bdys_202202.local_government_areas_analysis
-    group by lga_pid
+--), source_bdys as (
+--    select postcode,
+--           st_collect(geom) as geom
+--    from admin_bdys_202202.postcode_bdys
+--    where postcode is not null
+--    group by postcode
+--), target_bdys as (
+--    select lga_pid,
+--           st_collect(geom) as geom
+--    from admin_bdys_202202.local_government_areas_analysis
+--    group by lga_pid
 )
 select final.*,
---        null::geometry(polygon, 4283) as geom,
-       st_intersection(source_bdys.geom, target_bdys.geom) as geom
+        null::geometry(polygon, 4283) as geom
+--       st_intersection(source_bdys.geom, target_bdys.geom) as geom
 from final
-         inner join source_bdys on final.source_id = source_bdys.postcode
-         inner join target_bdys on final.target_id = target_bdys.lga_pid
+--         inner join source_bdys on final.source_id = source_bdys.postcode
+--         inner join target_bdys on final.target_id = target_bdys.lga_pid
 where percent_source_addresses > 0
 --    or percent_target_addresses > 0
 ;
@@ -192,6 +188,11 @@ alter table testing.concordance cluster on concordance_geom_idx;
 
 select count(*)
 from testing.concordance;
+
+
+select *
+from testing.concordance
+;
 
 
 
