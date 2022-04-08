fa5af7621012b4e8ff2c9a28e98f53f7978732c0
Hugh Saalmans <hugh.saalmans@iag.com.au>
2022-04-08 16:17:24 +1000

    added electoral bdys

--- 01_create_gnaf_census_bdys_table.sql
+++ 01_create_gnaf_census_bdys_table.sql
@@ -4,7 +4,7 @@
 -- step 1 - create a bunch of temp tables merging meshblocks with non-ABS structures (LGA, RAs etc...)
 -- use meshblock bdy centroids to get the bdy ID
 
--- create temp table of meshblock centroids (ensuring centroid is within polygon)
+-- create temp table of meshblock centroids (ensure centroid is within polygon by using ST_PointOnSurface)
 drop table if exists temp_mb;
 create temporary table temp_mb as
 select mb_16code,
@@ -15,7 +15,14 @@ analyse temp_mb;
 create index temp_mb_geom_idx on temp_mb using gist (geom);
 alter table temp_mb cluster on temp_mb_geom_idx;
 
--- get temp tables of meshblock IDs by boundary
+-- get temp tables of meshblock IDs per boundary
+
+drop table if exists temp_ced_mb;
+create temporary table temp_ced_mb as
+select distinct temp_mb.mb_16code, bdy.ced_code16 as ced_16code, bdy.ced_name16 as ced_16name from temp_mb
+inner join census_2016_bdys.ced_2016_aust as bdy on st_intersects(temp_mb.geom, bdy.geom);
+analyse temp_ced_mb;
+
 drop table if exists temp_lga_mb;
 create temporary table temp_lga_mb as
 select distinct temp_mb.mb_16code, bdy.lga_code16 as lga_16code, bdy.lga_name16 as lga_16name from temp_mb
@@ -33,6 +40,12 @@ create temporary table temp_ra_mb as
 select distinct temp_mb.mb_16code, bdy.ra_code16 as ra_16code, bdy.ra_name16 as ra_16name from temp_mb
 inner join census_2016_bdys.ra_2016_aust as bdy on st_intersects(temp_mb.geom, bdy.geom);
 analyse temp_ra_mb;
+
+drop table if exists temp_sed_mb;
+create temporary table temp_sed_mb as
+select distinct temp_mb.mb_16code, bdy.sed_code16 as sed_16code, bdy.sed_name16 as sed_16name from temp_mb
+inner join census_2016_bdys.sed_2016_aust as bdy on st_intersects(temp_mb.geom, bdy.geom);
+analyse temp_sed_mb;
 
 drop table if exists temp_ucl_mb;
 create temporary table temp_ucl_mb as
@@ -60,20 +73,26 @@ with abs as (
            sa4_16name,
            gcc_16code,
            gcc_16name,
+           ced_16code,
+           ced_16name,
            lga_16code,
            lga_16name,
            poa_16code,
            poa_16name,
            ra_16code,
            ra_16name,
+           sed_16code,
+           sed_16name,
            ucl_16code,
            ucl_16name,
            mb.state
     from admin_bdys_202202.abs_2016_mb as mb
-        inner join temp_lga_mb as lga on lga.mb_16code = mb.mb_16code
-        inner join temp_poa_mb as poa on poa.mb_16code = mb.mb_16code
-        inner join temp_ra_mb as ra on ra.mb_16code = mb.mb_16code
-        inner join temp_ucl_mb as ucl on ucl.mb_16code = mb.mb_16code
+             inner join temp_ced_mb as ced on ced.mb_16code = mb.mb_16code
+             inner join temp_lga_mb as lga on lga.mb_16code = mb.mb_16code
+             inner join temp_poa_mb as poa on poa.mb_16code = mb.mb_16code
+             inner join temp_sed_mb as sed on sed.mb_16code = mb.mb_16code
+             inner join temp_ra_mb as ra on ra.mb_16code = mb.mb_16code
+             inner join temp_ucl_mb as ucl on ucl.mb_16code = mb.mb_16code
 )
 select gid,
        gnaf_pid,
@@ -87,23 +106,14 @@ analyse gnaf_202202.address_principal_census_2016_boundaries;
 alter table gnaf_202202.address_principal_census_2016_boundaries add constraint address_principal_census_2016_boundaries_pkey primary key (gnaf_pid);
 alter table gnaf_202202.address_principal_census_2016_boundaries cluster on address_principal_census_2016_boundaries_pkey;
 
+drop table if exists temp_ced_mb;
 drop table if exists temp_lga_mb;
 drop table if exists temp_poa_mb;
 drop table if exists temp_ra_mb;
 drop table if exists temp_ucl_mb;
-
-
+drop table if exists temp_sed_mb;
 
 
 select * from gnaf_202202.address_principal_census_2016_boundaries
 ;
-
-
--- select count(*) from gnaf_202202.address_principal_census_2016_boundaries; -- 14451352
---
--- select count(*) from admin_bdys_202202.abs_2016_mb; -- 358011
--- select count(*) from temp_lga_mb; -- 358011
--- select count(*) from temp_poa_mb; -- 358011
--- select count(*) from temp_ra_mb; -- 358011
--- select count(*) from temp_ucl_mb; -- 358011
 
