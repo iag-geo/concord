215052d6c70c91b32a6429602aeda0bcbf88ad7d
Hugh Saalmans <hugh.saalmans@iag.com.au>
2022-04-27 13:13:49 +1000

    testing geoscape buildings

--- testing/xx_geoscape_testing.sql
+++ testing/xx_geoscape_testing.sql
@@ -51,6 +51,88 @@ from merge
 -- +--------------+--------------+-----------+---------------+-------------------------+
 
 
+-- create temp table of gnaf points with a buildings flag
+drop table if exists geoscape_202203.address_principals_buildings;
+create table geoscape_202203.address_principals_buildings as
+with blg as (
+    select adr.address_detail_pid as gnaf_pid,
+           coalesce(blgs.planning_zone, 'unknown') as planning_zone,
+           count(*)           as building_count
+    from geoscape_202203.building_address as adr
+    inner join geoscape_202203.buildings as blgs on blgs.building_pid = adr.building_pid
+    group by address_detail_pid,
+             planning_zone
+), merge as (
+select gnaf.gnaf_pid,
+       gnaf.reliability,
+       gnaf.state,
+       planning_zone,
+       coalesce(building_count, 0) as building_count,
+       geom
+from gnaf_202202.address_principals as gnaf
+    left outer join blg on blg.gnaf_pid = gnaf.gnaf_pid
+)
+select gnaf_pid,
+       reliability,
+       state,
+       string_agg(planning_zone, ' - ') as planning_zone,
+       sum(building_count) as building_count,
+       geom
+from merge
+    group by gnaf_pid,
+             reliability,
+             state,
+             geom
+;
+analyse geoscape_202203.address_principals_buildings;
+
+alter table geoscape_202203.address_principals_buildings add constraint address_principals_buildings_pkey primary key (gnaf_pid);
+create index address_principals_buildings_geom_idx on geoscape_202203.address_principals_buildings using gist (geom);
+alter table geoscape_202203.address_principals_buildings cluster on address_principals_buildings_geom_idx;
+
+
+-- compare planning_zone with meshblock category
+with gnaf1 as (
+    select gnaf_pid,
+           case when lower(planning_zone) LIKE '%residential%'
+                    or lower(planning_zone) LIKE '%mixed use%'
+                then 'residential'
+           end as is_residential,
+           planning_zone
+    from geoscape_202203.address_principals_buildings
+)
+select gnaf1.gnaf_pid,
+       gnaf1.is_residential,
+       gnaf1.planning_zone,
+       gnaf2.mb_category
+from gnaf1
+inner join gnaf_202202.address_principal_census_2016_boundaries as gnaf2 on gnaf2.gnaf_pid = gnaf1.gnaf_pid
+where gnaf1.is_residential = lower(gnaf2.mb_category)
+;
+
+
+
+
+
+
+select reliability,
+       count(*) as address_count
+from geoscape_202203.address_principals_buildings
+where building_count = 0
+group by reliability
+order by reliability
+;
+
+select planning_zone,
+       count(*) as address_count
+from geoscape_202203.address_principals_buildings
+group by planning_zone
+order by planning_zone
+;
+
+
+
+
 select *
 from geoscape_202203.buildings;
 
