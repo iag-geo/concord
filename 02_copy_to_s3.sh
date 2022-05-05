a48a01ecd101d7ac27e616987e3a7cbb45753124
Hugh Saalmans <hugh.saalmans@iag.com.au>
2022-05-05 10:49:24 +1000

    changed url to concordance CSV file

--- 02_copy_to_s3.sh
+++ 02_copy_to_s3.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-AWS_PROFILE="minus34"
+AWS_PROFILE="default"
 FILE_PATH="/Users/$(whoami)/git/iag_geo/concord/data"
 
-aws --profile=${AWS_PROFILE} s3 sync ${FILE_PATH} s3://minus34.com/opendata/geoscape-202202 --exclude "*" --include "*.csv" --acl public-read
+aws --profile=${AWS_PROFILE} s3 sync ${FILE_PATH} s3://minus34.com/opendata/geoscape-202202 \
+--exclude "*" --include "*.csv" --acl public-read
