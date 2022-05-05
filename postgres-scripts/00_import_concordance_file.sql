889bf16c7bbdfca58a2ec5160e7d28730900d225
Hugh Saalmans <hugh.saalmans@iag.com.au>
2022-05-05 11:23:56 +1000

    moved primary key creation

--- postgres-scripts/00_import_concordance_file.sql
+++ postgres-scripts/00_import_concordance_file.sql
@@ -4,22 +4,25 @@ drop table if exists gnaf_202202.boundary_concordance;
 create table gnaf_202202.boundary_concordance
 (
     from_source     text not null,
-    from_bdy       text not null,
+    from_bdy        text not null,
     from_id         text not null,
     from_name       text not null,
     to_source       text not null,
-    to_bdy         text not null,
+    to_bdy          text not null,
     to_id           text not null,
     to_name         text not null,
     address_count   integer,
-    address_percent double precision,
-    primary key (from_id, to_id)
+    address_percent double precision
 );
 alter table gnaf_202202.boundary_concordance owner to postgres;
 
--- import from CSV file
+-- import CSV file -- 586,977 rows affected in 1 s 365 ms
 COPY gnaf_202202.boundary_concordance
-    FROM '/Users/minus34/git/iag_geo/concord/data/boundary_concordance.csv'
+    FROM '/Users/s57405/git/iag_geo/concord/data/boundary_concordance.csv'
     WITH (HEADER, DELIMITER ',', FORMAT CSV);
 
 analyse gnaf_202202.boundary_concordance;
+
+-- add primary key (faster if done after import) -- completed in 8 s 496 ms
+alter table gnaf_202202.boundary_concordance add constraint boundary_concordance_pkey
+    primary key (from_source, from_bdy, from_id, to_source, to_bdy, to_id);
