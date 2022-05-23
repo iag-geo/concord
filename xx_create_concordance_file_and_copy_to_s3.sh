186fb8c56170c10436cf9a598627986f89c40c94
Hugh Saalmans <hugh.saalmans@gmail.com>
2022-05-23 15:29:07 +1000

    added runtime arguments

--- xx_create_concordance_file_and_copy_to_s3.sh
+++ xx_create_concordance_file_and_copy_to_s3.sh
@@ -12,7 +12,7 @@ python3 /Users/$(whoami)/git/iag_geo/concord/create_concordance_file.py --output
 # sample arguments
 #python3 /Users/$(whoami)/git/iag_geo/concord/create_concordance_file.py --admin-schema="admin_bdys_202202" --output-schema="gnaf_202202" --output-path=${OUTPUT_FOLDER}
 
-# copy to GDA94 & GDA2020 folders as GDA2020 would be the same as the GDA94 files
+# copy concordance file to GDA94 & GDA2020 folders as GDA2020 would be the same as the GDA94 files
 aws --profile=${AWS_PROFILE} s3 sync ${OUTPUT_FOLDER} s3://minus34.com/opendata/geoscape-202202 --exclude "*" --include "*.csv" --acl public-read
 aws --profile=${AWS_PROFILE} s3 sync ${OUTPUT_FOLDER} s3://minus34.com/opendata/geoscape-202202-gda2020 --exclude "*" --include "*.csv" --acl public-read
 
