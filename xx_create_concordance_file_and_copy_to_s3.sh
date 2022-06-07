4b4e23d24875b558fcbe58e3bde7ed2e644e8f69
Hugh Saalmans <hugh.saalmans@iag.com.au>
2022-06-07 18:51:24 +1000

    updated score table

--- xx_create_concordance_file_and_copy_to_s3.sh
+++ xx_create_concordance_file_and_copy_to_s3.sh
@@ -4,7 +4,7 @@ conda activate geo
 
 AWS_PROFILE="minus34"
 OUTPUT_FOLDER="/Users/$(whoami)/tmp/geoscape_202205"
-#OUTPUT_FOLDER_2020="/Users/$(whoami)/tmp/geoscape_202205_gda2020"
+OUTPUT_FOLDER_2020="/Users/$(whoami)/tmp/geoscape_202205_gda2020"
 
 mkdir -p "${OUTPUT_FOLDER}"
 mkdir -p "${OUTPUT_FOLDER_2020}"
@@ -12,11 +12,11 @@ mkdir -p "${OUTPUT_FOLDER_2020}"
 # Process using GDA94 boundaries
 python3 /Users/$(whoami)/git/iag_geo/concord/create_concordance_file.py --pgdb=geo --output-path=${OUTPUT_FOLDER}
 # Process using GDA2020 boundaries
-#python3 /Users/$(whoami)/git/iag_geo/concord/create_concordance_file.py --pgdb=geo --admin-schema="admin_bdys_202205_gda2020" --gnaf-schema="gnaf_202205_gda2020" --output-path=${OUTPUT_FOLDER_2020}
+python3 /Users/$(whoami)/git/iag_geo/concord/create_concordance_file.py --pgdb=geo --admin-schema="admin_bdys_202205_gda2020" --gnaf-schema="gnaf_202205_gda2020" --output-path=${OUTPUT_FOLDER_2020}
 
 # copy concordance files to GDA94 & GDA2020 folders as GDA2020 would be the same as the GDA94 files
 aws --profile=${AWS_PROFILE} s3 sync ${OUTPUT_FOLDER} s3://minus34.com/opendata/geoscape-202205 --exclude "*" --include "*.csv" --acl public-read
-#aws --profile=${AWS_PROFILE} s3 sync ${OUTPUT_FOLDER_2020} s3://minus34.com/opendata/geoscape-202205-gda2020 --exclude "*" --include "*.csv" --acl public-read
+aws --profile=${AWS_PROFILE} s3 sync ${OUTPUT_FOLDER_2020} s3://minus34.com/opendata/geoscape-202205-gda2020 --exclude "*" --include "*.csv" --acl public-read
 
 # copy GDA94 score file to GitHub repo local files
 cp ${OUTPUT_FOLDER}/boundary_concordance_score.csv /Users/$(whoami)/git/iag_geo/concord/data/
