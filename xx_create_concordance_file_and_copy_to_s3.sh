634d077f81d0582385531cd13c06b50bbb0799f3
Hugh Saalmans <hugh.saalmans@gmail.com>
2024-05-15 08:55:06 +1000

    Updates for 202405 release

--- xx_create_concordance_file_and_copy_to_s3.sh
+++ xx_create_concordance_file_and_copy_to_s3.sh
@@ -3,8 +3,8 @@
 conda activate geo
 
 AWS_PROFILE="minus34"
-OUTPUT_FOLDER="/Users/$(whoami)/tmp/geoscape_202402"
-OUTPUT_FOLDER_2020="/Users/$(whoami)/tmp/geoscape_202402_gda2020"
+OUTPUT_FOLDER="/Users/$(whoami)/tmp/geoscape_202405"
+OUTPUT_FOLDER_2020="/Users/$(whoami)/tmp/geoscape_202405_gda2020"
 
 mkdir -p "${OUTPUT_FOLDER}"
 mkdir -p "${OUTPUT_FOLDER_2020}"
@@ -12,11 +12,11 @@ mkdir -p "${OUTPUT_FOLDER_2020}"
 # Process using GDA94 boundaries
 python3 /Users/$(whoami)/git/iag_geo/concord/create_concordance_file.py --pgdb=geo --output-path=${OUTPUT_FOLDER}
 # Process using GDA2020 boundaries
-python3 /Users/$(whoami)/git/iag_geo/concord/create_concordance_file.py --pgdb=geo --admin-schema="admin_bdys_202402_gda2020" --gnaf-schema="gnaf_202402_gda2020" --output-path=${OUTPUT_FOLDER_2020}
+python3 /Users/$(whoami)/git/iag_geo/concord/create_concordance_file.py --pgdb=geo --admin-schema="admin_bdys_202405_gda2020" --gnaf-schema="gnaf_202405_gda2020" --output-path=${OUTPUT_FOLDER_2020}
 
 # copy concordance files to GDA94 & GDA2020 folders as GDA2020 would be the same as the GDA94 files
-aws --profile=${AWS_PROFILE} s3 sync ${OUTPUT_FOLDER} s3://minus34.com/opendata/geoscape-202402 --exclude "*" --include "*.csv" --acl public-read
-aws --profile=${AWS_PROFILE} s3 sync ${OUTPUT_FOLDER_2020} s3://minus34.com/opendata/geoscape-202402-gda2020 --exclude "*" --include "*.csv" --acl public-read
+aws --profile=${AWS_PROFILE} s3 sync ${OUTPUT_FOLDER} s3://minus34.com/opendata/geoscape-202405 --exclude "*" --include "*.csv" --acl public-read
+aws --profile=${AWS_PROFILE} s3 sync ${OUTPUT_FOLDER_2020} s3://minus34.com/opendata/geoscape-202405-gda2020 --exclude "*" --include "*.csv" --acl public-read
 
 # copy GDA94 score file to GitHub repo local files
 cp ${OUTPUT_FOLDER}/boundary_concordance_score.csv /Users/$(whoami)/git/iag_geo/concord/data/
